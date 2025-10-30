9ba8b597c1e36c07a12142c88fb6864ec7d15a42
EmilianoCBE <e.carrizales1305@gmail.com>
2025-10-29 21:50:41 -0600

    Limpia de Router y DashboardLayout

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
 import './DashboardLayout.css';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
+import { Outlet } from 'react-router-dom';
 
-const DashboardLayout = ({ children}) => {
+const DashboardLayout = () => {
   return (
     <div className="container">
       <Sidebar/>
       <div className="mainContainer">
         <Header />
         <div className="content">
-          {children}
+          <Outlet />
         </div>
       </div>
     </div>
